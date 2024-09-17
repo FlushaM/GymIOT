b7160ec715102b1299e6f87cb447a67a3c49e7df
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-09-17 18:14:16 -0300

    Initial commit

---
+++ app/src/main/java/com/example/gymiot/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.gymiot.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gymiot.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
