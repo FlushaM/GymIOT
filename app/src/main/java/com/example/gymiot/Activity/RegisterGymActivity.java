0e33beeae30f6663ec28179549443f19f54f92d5
FlushaM <83796763+FlushaM@users.noreply.github.com>
2024-10-24 03:12:40 -0300

    afinado register

--- app/src/main/java/com/example/gymiot/Activity/RegisterGymActivity.java
+++ app/src/main/java/com/example/gymiot/Activity/RegisterGymActivity.java
@@ -32,15 +32,15 @@ import java.util.UUID;
 public class RegisterGymActivity extends AppCompatActivity {
 
     private CheckBox checkBoxLunes, checkBoxMartes, checkBoxMiercoles, checkBoxJueves, checkBoxViernes, checkBoxSabado, checkBoxDomingo;
-    private EditText openingTimeEdt, closingTimeEdt;
-
-    private static final int PICK_IMAGE_REQUEST = 1;
+    private EditText openingTimeEdt, closingTimeEdt, locationUrlEdt; // Nuevo campo para la URL de Google Maps
+    private static final int PICK_IMAGE_REQUEST = 2;
     private Uri imageUri;
+    private Uri selectedImageUri;
     private ImageView gymProfileImage;
     private FirebaseFirestore db;
     private StorageReference storageRef;
-
     private FirebaseAuth mAuth; // Para obtener el UID del usuario
+
     // Spinners
     private Spinner spinnerPais;
     private Spinner spinnerRegion;
@@ -62,6 +62,7 @@ public class RegisterGymActivity extends AppCompatActivity {
         checkBoxDomingo = findViewById(R.id.checkBoxDomingo);
         openingTimeEdt = findViewById(R.id.openingTimeEdt);
         closingTimeEdt = findViewById(R.id.closingTimeEdt);
+        locationUrlEdt = findViewById(R.id.locationUrlEdt); // Inicializar el campo de URL
 
         db = FirebaseFirestore.getInstance();
         storageRef = FirebaseStorage.getInstance().getReference();
@@ -135,14 +136,15 @@ public class RegisterGymActivity extends AppCompatActivity {
 
         registerGymBtn.setOnClickListener(v -> {
             String gymName = gymNameEdt.getText().toString().trim();
-            String ciudad = spinnerCiudad.getSelectedItem().toString(); // Obtener ciudad del spinner
-            String region = spinnerRegion.getSelectedItem().toString(); // Obtener región del spinner
-            String pais = spinnerPais.getSelectedItem().toString(); // Obtener país del spinner
+            String ciudad = spinnerCiudad.getSelectedItem().toString();
+            String region = spinnerRegion.getSelectedItem().toString();
+            String pais = spinnerPais.getSelectedItem().toString();
             String calle = calleEdt.getText().toString().trim();
             String mensualidad = monthlyPriceEdt.getText().toString().trim();
             String diario = dailyPriceEdt.getText().toString().trim();
-            String openingTime = openingTimeEdt.getText().toString().trim(); // Hora de apertura
-            String closingTime = closingTimeEdt.getText().toString().trim(); // Hora de cierre
+            String openingTime = openingTimeEdt.getText().toString().trim();
+            String closingTime = closingTimeEdt.getText().toString().trim();
+            String locationUrl = locationUrlEdt.getText().toString().trim(); // Obtener URL de ubicación
 
             // Días de operación seleccionados
             ArrayList<String> diasDisponibles = new ArrayList<>();
@@ -166,7 +168,7 @@ public class RegisterGymActivity extends AppCompatActivity {
             String userId = mAuth.getCurrentUser().getUid();
 
             if (imageUri != null) {
-                uploadImageToFirebase(gymName, ciudad, region, pais, calle, mensualidad, diario, openingTime, closingTime, maquinasDisponibles, diasDisponibles, userId);
+                uploadImageToFirebase(gymName, ciudad, region, pais, calle, mensualidad, diario, openingTime, closingTime, maquinasDisponibles, diasDisponibles, locationUrl, userId);
             } else {
                 Toast.makeText(this, "Por favor, sube una imagen de perfil", Toast.LENGTH_SHORT).show();
             }
@@ -203,17 +205,17 @@ public class RegisterGymActivity extends AppCompatActivity {
         }
     }
 
-    private void uploadImageToFirebase(String gymName, String ciudad, String region, String pais, String calle, String mensualidad, String diario, String openingTime, String closingTime, ArrayList<String> maquinasDisponibles, ArrayList<String> diasDisponibles, String userId) {
+    private void uploadImageToFirebase(String gymName, String ciudad, String region, String pais, String calle, String mensualidad, String diario, String openingTime, String closingTime, ArrayList<String> maquinasDisponibles, ArrayList<String> diasDisponibles, String locationUrl, String userId) {
         StorageReference fileReference = storageRef.child("gym_images/" + UUID.randomUUID().toString() + ".jpg");
         fileReference.putFile(imageUri)
                 .addOnSuccessListener(taskSnapshot -> fileReference.getDownloadUrl().addOnSuccessListener(uri -> {
                     String imageUrl = uri.toString();
-                    registerGym(gymName, ciudad, region, pais, calle, mensualidad, diario, openingTime, closingTime, maquinasDisponibles, diasDisponibles, imageUrl, userId);
+                    registerGym(gymName, ciudad, region, pais, calle, mensualidad, diario, openingTime, closingTime, maquinasDisponibles, diasDisponibles, imageUrl, locationUrl, userId);
                 }))
                 .addOnFailureListener(e -> Toast.makeText(RegisterGymActivity.this, "Error al subir la imagen: " + e.getMessage(), Toast.LENGTH_SHORT).show());
     }
 
-    private void registerGym(String gymName, String ciudad, String region, String pais, String calle, String mensualidad, String diario, String openingTime, String closingTime, ArrayList<String> maquinasDisponibles, ArrayList<String> diasDisponibles, String imageUrl, String userId) {
+    private void registerGym(String gymName, String ciudad, String region, String pais, String calle, String mensualidad, String diario, String openingTime, String closingTime, ArrayList<String> maquinasDisponibles, ArrayList<String> diasDisponibles, String imageUrl, String locationUrl, String userId) {
         Map<String, Object> gymData = new HashMap<>();
         gymData.put("gymName", gymName);
         gymData.put("ciudad", ciudad);
@@ -222,19 +224,18 @@ public class RegisterGymActivity extends AppCompatActivity {
         gymData.put("calle", calle);
         gymData.put("mensualidad", mensualidad);
         gymData.put("diario", diario.isEmpty() ? "0" : diario);
-        gymData.put("horarioApertura", openingTime); // Guardar hora de apertura
-        gymData.put("horarioCierre", closingTime); // Guardar hora de cierre
-        gymData.put("diasDisponibles", diasDisponibles); // Guardar los días seleccionados
+        gymData.put("horarioApertura", openingTime);
+        gymData.put("horarioCierre", closingTime);
+        gymData.put("diasDisponibles", diasDisponibles);
         gymData.put("maquinasDisponibles", maquinasDisponibles);
         gymData.put("imageUrl", imageUrl);
-        gymData.put("ownerId", userId); // Asociar el UID del usuario al gimnasio
+        gymData.put("locationUrl", locationUrl); // Guardar URL de ubicación
+        gymData.put("ownerId", userId);
 
-        // Guardar el gimnasio en Firestore
         db.collection("gyms").add(gymData)
                 .addOnSuccessListener(documentReference -> {
                     String gymId = documentReference.getId();
 
-                    // Asociar el gymId con el perfil del usuario
                     db.collection("users").document(userId)
                             .update("gymId", gymId)
                             .addOnSuccessListener(aVoid -> {
